2809e81a3ef5a3efa8600c0e1853939cb66a491b
xdhuxc <xdhuxc@163.com>
2018-07-31 21:10:55 +0800

    调用marathon接口。

--- util/marathon.py
+++ util/marathon.py
@@ -3,6 +3,12 @@
 
 import requests
 import json
+import sys
+
+charset = 'utf-8'
+
+reload(sys)
+sys.setdefaultencoding(charset)
 
 marathon_url = "http://172.20.17.4:8080"
 
@@ -10,6 +16,20 @@ marathon_url = "http://172.20.17.4:8080"
 def get_all_apps():
     request_url = marathon_url + "/v2/apps"
     resp = requests.get(request_url)
-    apps = j
+    result_code = resp.status_code
+    if result_code == 200:
+        # 获取所有的应用列表
+        rt = json.loads(resp.content, encoding=charset)['apps']
+        for item in rt:
 
+
+            app_id = item['id']
+            if app_id.startswith('/dcee'):
+                app_name = app_id[6:]
+                print('app_id: %s, app_name: %s' % (app_id, app_name))
+
+
+if __name__ == '__main__':
+    print(sys.getdefaultencoding())
+    get_all_apps()
 
