5675aaf5e2f425f92997b230e33765555a3b259a
xdhuxc <xdhuxc@163.com>
2018-08-09 09:07:45 +0800

    获取CPU信息。

--- practice/xdhuxc_cmdn.py
+++ practice/xdhuxc_cmdn.py
@@ -6,12 +6,14 @@ import subprocess
 # https://blog.csdn.net/huguangshanse00/article/details/17053789
 # http://blog.51cto.com/dragonball/1413369
 # https://blog.csdn.net/dearbaba_8520/article/details/80662181
+# https://blog.csdn.net/huguangshanse00/article/details/17053891 含/proc/stat文件中参数含义
+# https://blog.csdn.net/swiftshow/article/details/8109322 获取全部进程的资源信息
 
 
 def memory_stat():
     """
     用于计算内存占用状况
-    :return: memory 字典，包含当前机器所有内存信息
+    :return: memory 字典，包含当前机器所有内存参数及当前值信息，单位为：B。
     """
     memory = {}
     with open('C:\\Users\\wanghuan\\Desktop\\meminfo') as men_info:
@@ -25,9 +27,24 @@ def memory_stat():
     return memory
 
 
-def cpu_stat():
-    print()
+def cpu_hardware_stat():
+    cpu_hardware = []
+    per_cpu = {}
+    with open('') as cpu_hardware_info:
+        for line in cpu_hardware_info:
+            if line == '\n':
+                cpu_hardware.append(per_cpu)
+                per_cpu = {}
+            cpu_hardware_key = line.split(':')[0].rstrip()  # 删除字符串末尾指定的字符，默认为空格。
+            cpu_hardware_value = line.split(':')[1]
+            per_cpu[cpu_hardware_key] = cpu_hardware_value
 
+    return cpu_hardware
+
+
+def cpu_use_stat():
+
+    print()
 
 
 if __name__ == '__main__':
